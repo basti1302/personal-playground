cb4bd2cf37072c8c854b241241bb4b6c52a90bee
Bastian Krol <bastian.krol@web.de>
2015-08-16 03:09:28 +0200

    only toggle two classes

--- js/entry.js
+++ js/entry.js
@@ -5,11 +5,7 @@ import $ from 'jquery';
 
 $(function() {
   $('.border').click(() => {
-    $('.left').toggleClass('left-hide');
-    $('.left').toggleClass('left-show');
-    $('.right').toggleClass('right-half');
-    $('.right').toggleClass('right-full');
-    $('.toggle').toggleClass('toggle-right');
-    $('.toggle').toggleClass('toggle-left');
+    $('.wrapper').toggleClass('panel-hidden');
+    $('.wrapper').toggleClass('panel-shown');
   });
 });
